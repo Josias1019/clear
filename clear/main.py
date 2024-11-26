8d9e369aa00a97405847451ffbf58651fc73eb97
Josias1019 <128244240+Josias1019@users.noreply.github.com>
2024-11-25 22:12:51 -0300

    main.py

--- clear/main.py
+++ clear/main.py
@@ -2,6 +2,8 @@ import imaplib
 import email
 from email.header import decode_header
 
+
+
 # Função para decodificar o assunto com segurança
 def decodificador(subject):
     try:
@@ -13,6 +15,8 @@ def decodificador(subject):
         print(f"Erro ao decodificar o assunto: {e}")
         return subject
 
+
+
 # Função para detectar a pasta de lixeira no Gmail
 def encontrar_pasta_lixeira(mail):
     status, arquivos = mail.list()
@@ -22,14 +26,19 @@ def encontrar_pasta_lixeira(mail):
                 return arquivo.decode().split(' "/" ')[-1].strip('"')
     return None
 
-# Configurações de login e servidor
+
+
 IMAP_SERVER = "imap.gmail.com"
 EMAIL = "josiascraft1019@gmail.com"
 PASSWORD = "ager ofnm nnpl kudc"
 
+
+
 # Intervalo de datas desejado
 inicio_data = "01-Jan-2017"  # Desde 01/01/2017
 fim_data = "25-Nov-2024"  # Até 25/11/2024
+
+
 
 # Conexão ao servidor
 try:
@@ -59,6 +68,7 @@ try:
         try:
             # Obtém o e-mail
             status, msg_data = mail.fetch(mail_id, "(RFC822)")
+            
             for response_part in msg_data:
                 if isinstance(response_part, tuple):
                     # Decodifica o e-mail
@@ -68,25 +78,12 @@ try:
                     subject = decodificador(msg["Subject"])
                     if isinstance(subject, str):
                         print(f"Assunto: {subject}")
+                        
+                        # lista extensa de palavras-chave define os critérios de "spam"
                         keywords = [
-                            "c-date", "stellantis", "promoção", "linkedin", "freelancer", "epic games", "jobbol", "infojobs", 
-                            "pinterest", "nubank", "mercado pago", "vagas estagios", "Cataratas", "Steam", "descontos", "B3", 
-                            "kwai", "snapchat", "udemy", "pix", "dinheiro", "InsaneGGameS", "yoda", "vagas", "estágiario",
-                            "emprego", "cupom", "twitch", "live", "compras", "viagem", "grátis", "pesquisa", "ganhe", "oferta",
-                            "compartilhou", "publicações", "perfil", "marketing", "off", "mensagem", "like", "try", "us", "ganhar", "conversar",
-                            "%", "convite", "R$", "cargo", "contratando", "oportunidades", "miss", "sent", "vaga", "auxiliar", "mensagem", "free",
-                            "Técnico", "Desenvolvedor", "Estagiário", "contratando", "Solicitação", "off", "cargo", "curso", "mensagens", "League of Legends",
-                            "kabum", "sorte", "opinião", "Aprenda", "Ninja", "Vivo", "$", "black friday", "sale", "Assistente", "Ubisoft", "Feedback",
-                            "convites", "currículo", "spam", "Publicidade", "Points", "Music", "Educacional", "lol", "@Rappi", "GitHub", "Oi", "Last", "financeiro",
-                            "Novidade", "XP Investimentos", "Invista", "destaques", "canal", "Oportunidade", "Netflix", "feriados", "Dicas",
-                            "retrospectiva", "dúvidas", "filme", "Fantia", "inscrição", "limite", "Continue", "SSGamers", "Claim", "desconto",
-                            "série", "notificações", "Full", "CodinGame", "summer", "crédito", "cupom", "Power BI", "PERCA", "Lembrete",
-                            "Aproveite", "PRÉ-MATRICULA", "Updates", "Treinamento", "Pokémon", "acaba", "novo", "destrave", "inglês",
-                            "Olá", "CPF", "negativado", "Atualizações", "incríveis", "promo", "99Freelas" "Novo Projeto",
-                            "jogo", "tiktok", "vídeos", "pagamento", "selecionado", "Projeto", "comentou", "Aprenda", "Fiat", "contratou"
-                            "sugestões", "YouTube", "Cripto", "reagiu", "preço", "HTML", "PHP", "Website", "chegou", "Jeep",
-                            "Últimos"
+                            "propaganda", "SPAM", "promoção", 
                         ]
+                        
                         if any(keyword in subject.lower() for keyword in keywords):
                             print("E-mail identificado como spam. Movendo para a lixeira...")
                             # Move para a lixeira
